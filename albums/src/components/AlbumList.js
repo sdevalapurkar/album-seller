d3708078b4dd8bc14acfdee54aabf6daf6145e9d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2017-06-08 14:18:55 -0500

    added albumdetail and albumlist components to display the list of albums from api

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,53 @@
 //import libraries
-import React from 'react';
+import React, { Component } from 'react';
 import {
-    Text,
     View
 } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 //make component
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>    
-    );
-};
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        fetch('http://rallycoding.herokuapp.com/api/music_albums', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result); 
+            this.setState({
+                albums: result
+            });   
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>    
+        );
+    }
+}
 
 //make component available to other parts of the application
 export default AlbumList;

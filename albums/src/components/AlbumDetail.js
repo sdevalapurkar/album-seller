11e96a663ede29debf9887bfc7ed9b132b83b192
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2017-06-09 10:49:14 -0500

    adding style with flexbox to albumdetail

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
-    Text
+    Text,
+    View,
+    StyleSheet
 } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
@@ -9,10 +11,23 @@ const AlbumDetail = (props) => {
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View> 
+
+                </View>
+                <View style={styles.headerContent}>    
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>    
             </CardSection>    
         </Card>        
     );
 };
+
+const styles = StyleSheet.create({
+    headerContent: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
+});
 
 export default AlbumDetail;
